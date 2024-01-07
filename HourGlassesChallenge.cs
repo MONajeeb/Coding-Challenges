1e98f1cfa6f62432e57d16449b24150461bdf0cb
MO_Najeeb <mohamednajeeb300@gmail.com>
2024-01-07 20:50:29 +0200

    Add Palindrome challenge

--- HourGlassesChallenge.cs
+++ HourGlassesChallenge.cs
@@ -10,52 +10,52 @@ using System.Xml.Linq;
 
 class Solution
 {
-    public static void Main(string[] args)
-    {
-        List<List<int>> arr = new List<List<int>>();
+    //public static void Main(string[] args)
+    //{
+    //    List<List<int>> arr = new List<List<int>>();
 
-        for (int i = 0; i < 6; i++)
-        {
-            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
-        }
-        List<List<List<int>>> resultHourGlasses = new List<List<List<int>>>();
-        List<int> sums = new List<int>();
+    //    for (int i = 0; i < 6; i++)
+    //    {
+    //        arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+    //    }
+    //    List<List<List<int>>> resultHourGlasses = new List<List<List<int>>>();
+    //    List<int> sums = new List<int>();
 
-        for (int x = 0; x <= 3; x++)
-        {
-            for (int y = 0; y <= 3; y++)
-            {
-                var hourGlass = GetHourGlass(new List<List<int>> { arr[x].GetRange(y, 3), arr[x + 1].GetRange(y, 3), arr[x + 2].GetRange(y, 3) });
-                resultHourGlasses.Add(hourGlass);
-            }
-        }
-
-
-        foreach (var item in resultHourGlasses)
-        {
-            //Console.WriteLine(GetHourGlassSum(item));
-            sums.Add(GetHourGlassSum(item));
-        }
-        Console.WriteLine(sums.Max());
-
-    }
-    public static List<List<int>> GetHourGlass(List<List<int>> Arr)
-    {
+    //    for (int x = 0; x <= 3; x++)
+    //    {
+    //        for (int y = 0; y <= 3; y++)
+    //        {
+    //            var hourGlass = GetHourGlass(new List<List<int>> { arr[x].GetRange(y, 3), arr[x + 1].GetRange(y, 3), arr[x + 2].GetRange(y, 3) });
+    //            resultHourGlasses.Add(hourGlass);
+    //        }
+    //    }
 
 
-        var hourGlass = new List<List<int>>
-        {
-            Arr[0],
-            new List<int> { Arr[1][1] },
-            Arr[2]
-        };
-        return hourGlass;
+    //    foreach (var item in resultHourGlasses)
+    //    {
+    //        //Console.WriteLine(GetHourGlassSum(item));
+    //        sums.Add(GetHourGlassSum(item));
+    //    }
+    //    Console.WriteLine(sums.Max());
 
-    }
+    //}
+    //public static List<List<int>> GetHourGlass(List<List<int>> Arr)
+    //{
 
 
-    public static int GetHourGlassSum(List<List<int>> Arr)
-    {
-        return (Arr[0].Sum() + Arr[1].Sum() + Arr[2].Sum());
-    }
+    //    var hourGlass = new List<List<int>>
+    //    {
+    //        Arr[0],
+    //        new List<int> { Arr[1][1] },
+    //        Arr[2]
+    //    };
+    //    return hourGlass;
+
+    //}
+
+
+    //public static int GetHourGlassSum(List<List<int>> Arr)
+    //{
+    //    return (Arr[0].Sum() + Arr[1].Sum() + Arr[2].Sum());
+    //}
 }
---
+++ Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Palindrome
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome(-1));
+    }
+        public static bool IsPalindrome(int input)
+    {
+        int temp = input;
+        int sum = 0;
+        while (input > 0)
+        {
+            int rem = input % 10;
+            sum = (sum * 10) + rem;
+            Console.WriteLine(rem);
+            input = input / 10;
+        }
+
+        if (temp == sum )
+        {
+            return true;
+        }
+        return false;
+
+    }
+}
+
